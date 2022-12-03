f538eb7e8315541c6a55bfe8b3887770d3203abc
Rajeskumar180599 <118203093+Rajeskumar180599@users.noreply.github.com>
2022-12-03 20:17:16 +0530

    src added

---
+++ src/Bike/Bike.java
@@ -0,0 +1,20 @@
+package Bike;
+
+public class Bike {
+	public void Ninja_H2r() {
+		System.out.println("Fastest bike in the World");
+	}
+	private void car() {
+System.out.println("Ferrari");
+	}
+	 void truck() {
+		System.out.println("Benz");
+
+	}
+public static void main(String[] args) {
+	Bike review=new Bike();
+	review.Ninja_H2r();
+	review.car();
+	review.truck();
+}
+}
